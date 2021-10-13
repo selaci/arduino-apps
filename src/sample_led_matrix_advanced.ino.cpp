0ee258f2310e2af373447e25e1ec285c288c57e7
Jordi Grant i Esteve <jgrant.esteve@gmail.com>
2021-10-13 20:16:48 +0100

    working on led matrix

--- src/sample_led_matrix_advanced.ino.cpp
+++ src/sample_led_matrix_advanced.ino.cpp
@@ -8,21 +8,21 @@
 
 #include <Arduino.h>
 
-const int SERIAL_BAUD_RATE = 115200;
+const int SERIAL_BAUD_RATE = 19200;
 const int ADDRESS          = 0x68;
 
 // Button.
 const int BUTTON_PIN       = 4;
 
 // 74HC595
-const int DATA_PIN         = 5;
-const int CLOCK_PIN        = 6;
-const int LATCH_PIN        = 7;
+const int DATA_PIN         = 8;
+const int CLOCK_PIN        = 9;
+const int LATCH_PIN        = 10;
 
 // TPIC6B595.
-const int SER_IN_PIN       = 8;
-const int SRCLK_PIN        = 9;
-const int RCK_PIN          = 10;
+const int SER_IN_PIN       = 13;
+const int SRCLK_PIN        = 11;
+const int RCK_PIN          = 12;
 
 ArduinoGear::GWire wire;
 ArduinoGear::Pin pin;
@@ -40,37 +40,38 @@ const uint8_t NUM_SEQUENCES = 6;
 const uint8_t SIZE = 8;
 
 int row_sequences[NUM_SEQUENCES][SIZE] = {
-  {1, 2, 4, 8, 18, 32, 64, 128},
+  {1, 2, 4, 8, 16, 32, 64, 128},
   {3, 7, 14, 28, 56, 112, 224, 192},
   {255, 255, 255, 255, 255, 255, 255, 255},
-  {1, 2, 4, 8, 18, 32, 64, 128},
+  {1, 2, 4, 8, 16, 32, 64, 128},
   {254, 253, 251, 247, 239, 223, 191, 127},
   {252, 248, 241, 227, 199, 143, 31, 127}
 };
 
 uint8_t column_sequences[NUM_SEQUENCES][SIZE] = {
-  {1, 2, 4, 8, 18, 32, 64, 128},
+  {1, 2, 4, 8, 16, 32, 64, 128},
   {3, 7, 14, 28, 56, 112, 224, 192},
-  {1, 2, 4, 8, 18, 32, 64, 128},
+  {1, 2, 4, 8, 16, 32, 64, 128},
   {255, 255, 255, 255, 255, 255, 255, 255},
   {254, 253, 251, 247, 239, 223, 191, 127},
   {252, 248, 241, 227, 199, 143, 31, 127}
 };
 
 // Mapping numbers.
-int rowMappingNumber;
-int columnMappingNumber;
+uint8_t rowMappingNumber;
+uint8_t columnMappingNumber;
 
 // Rows and column values. If bit is "1" then "on", otherwise "off".
 int rowEncoding = 0;
 int columnEncoding = 0;
 
 // Low pass filter (1st. order)
-float alpha = 0.5;
-int previousrowMappingNumber = 3;    // Kind of the center.
-int previouscolumnMappingNumber = 3; // Kind of the center.
+float alpha = 0;
+uint8_t previousrowMappingNumber = 3;    // Kind of the center.
+uint8_t previouscolumnMappingNumber = 3; // Kind of the center.
+uint8_t number; // A helper variable.
 
-uint8_t i; // A variable to iterate.
+uint8_t i; // A variable to ite  rate.
 uint8_t j = 0; // A veriable that contains what row to start with.
 uint8_t count = 0;
 uint8_t result = 0;
@@ -79,12 +80,25 @@ void setup()
 {
   Serial.begin(SERIAL_BAUD_RATE);
   mpu6050.init();
+
+  sr74hc595.setup();
+  srtpic6b595.setup();
+  button.setup();
+
+  Serial.println("Setup end.");
 }
 
+char buff[100];
+
+uint8_t cc = 0;
 void loop()
 {
   // Get a reading.
-  mpuReading = mpu6050.getAccReading();
+  if (cc == 0) {
+    mpuReading = mpu6050.getAccReading();
+  }
+
+  cc = (cc + 1) % 16;
 
   // Calculate row and column numbers.
   /*
@@ -98,10 +112,10 @@ void loop()
    *
    * Now, instead of using "22.5", I use "22.625". This guarantees than "-90" and "90" are within
    * a valid rage number. Otherwise, we'd have this:
-   * 
+   *
    *    90 / 22.5 =>  4
    *   -90 / 22.6 => -4
-   * 
+   *
    *   The overall range of row numbers is -4, -3, -2, -1, 0, 1, 2, 3, 4, which is 9 numbers instead
    *   of 8.
    *
@@ -116,12 +130,18 @@ void loop()
    *  [ 45  , 67.5 ) => 1
    *  [ 67.5, 90   ) => 0
    */
-  rowMappingNumber = (int) (7 - (mpuReading.x + 90) / 22.625);
-  columnMappingNumber = (int) (7 - (mpuReading.y + 90) / 22.625);
 
-  previousrowMappingNumber = (int) (rowMappingNumber + alpha * previousrowMappingNumber);
-  previouscolumnMappingNumber = (int) (columnMappingNumber + alpha * previouscolumnMappingNumber);
+  number = (mpuReading.x + 90) / 22.625;
+  rowMappingNumber = 7 - number;
 
+  number = (mpuReading.y + 90) / 22.625;
+  columnMappingNumber = 7 - number;
+
+  previousrowMappingNumber = (int) ((1 - alpha) * rowMappingNumber + alpha * previousrowMappingNumber);  
+
+  uint8_t tmp = previouscolumnMappingNumber;
+  previouscolumnMappingNumber = (int) ((1 - alpha) * columnMappingNumber + alpha * previouscolumnMappingNumber);
+  
   if (button.has_pressed()) {
     // Change app. sequence.
     sequence_index = (sequence_index + 1) % NUM_SEQUENCES;
@@ -131,32 +151,33 @@ void loop()
   rowEncoding = column_sequences[sequence_index][previouscolumnMappingNumber];
 
   // Turn on / off rows and columns. Multiple rows can be on at any given time.
-  srtpic6b595.shiftOut(columnEncoding);
+  
+  srtpic6b595.shiftOut(columnEncoding);  
 
   /*
    * Turn on/off rows.
    *
    * Only a single row can be on at any given time. When a row sequence wants more than one row on,
    * a row will be turned on first then off before turning next row on.
-   * 
+   *
    * This logic has an issue though; the last row to be turned on will have more time on compared to
    * any other row. That's because every iteration needs time to get a reading, calculate next
    * sequence and other tasks. When a row has more time than the others on, that row will show more,
    * which is undesired.
-   * 
+   *
    * In order to workaround this, the next logic does not start with the same row or finishes with
    * the same row either in every single loop. Each loop iteration will have a different row to
    * start with and each iteration will go through all the rows. This means that in every single
    * loop iteration a different row will be left on last, thus giving the same brightness to every
    * row.
-   * 
+   *
    * For example, the first time the loop starts, it will start by row 0 and leave row 7 last.
    * The next time, will start by row 1 and leave row 0 last. The next time will start by row 2 and
    * leave row 1 last and so forth.
-   * 
+   *
    * Lastly, if a row happens to be off the logic moves on. As soon as one row is one, all the other
    * rows will be turned off, so there is no need to explicitely set a row off.
-   */
+   */  
   while(true) {
     i = j;
     result = rowEncoding & (1 << i);
@@ -175,4 +196,5 @@ void loop()
 
   j = (j + 1) % SIZE; // Next loop iteration will start and finish with a different rows.
   count = 0;
+  
 }
